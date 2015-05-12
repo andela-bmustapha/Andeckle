c1d9c656dd7bd1e7ddba64ac078f2c4c01d0fce7
toystars <toystars2008@gmail.com>
2015-05-12 20:22:25 +0100

    Added the manual time logging functionality

--- js/andeckle-controllers.js
+++ js/andeckle-controllers.js
@@ -6,13 +6,14 @@ angular.module('andeckle', ['ngStorage'])
   // replace this with self
   var self = this;
 
+  // set the app tagline
+  self.appTag = 'Time management simplified...';
+
   //set up default local storage in case none is available
   self.completeData = {
     name: '',
     timeTagged: [
-      { tags: '#training #trainingInProgress', comment: 'Training', hour: 2, minute: 34, second: 25, date: 24/5/2015 },
-      { tags: '#angularPro', comment: 'Angular Project', hour: 5, minute: 5, second: 12, date: 5/2/2014 },
-      { tags: '#eating #food', comment: 'Eating', hour: 2, minute: 34, second: 25, date: 24/5/2015 }
+      { tags: '#training #trainingInProgress', comment: 'Training', hour: 2, minute: 34, date: 'Fri May 22 2015' }
     ]
   };
 
@@ -23,30 +24,70 @@ angular.module('andeckle', ['ngStorage'])
     self.completeData = $localStorage.completeData;
   }
 
-  // define models
-  self.timerHours = 0;
-  self.timerMinutes = 0;
-  self.timerSeconds = 0;
-  self.timerTags = '';
-  self.timerDescription = '';
-  self.timerDate = '';
+  console.log($localStorage.completeData);
 
-  self.userName = '';
+  // define models
+  self.timerHours = 0; // model for hours
+  self.timerMinutes = 0;  // model for minutes
+  self.timerTags = '';  // model for tags
+  self.timerDescription = '';  // model for description
+  self.timerDate = '';  // model for date
+
+  self.userName = '';  // model for name
+  self.newDateString = '';
+
+  self.checkData = function() {
+    if ((typeof self.timerHours === 'number' && self.timerHours > 0) && (typeof self.timerMinutes === 'number' && self.timerMinutes > 0) && (typeof self.timerTags === 'string' && self.timerTags.length > 0) && (typeof self.timerDate === 'object') && (typeof self.timerDescription === 'string' && self.timerDescription.length > 0)) {//&& (typeof self.timerMinutes !== 'number') && self.timerTags !== '' && self.timerDescription !== '' && self.timerDate !== '')
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  self.checkInvalid = function() {
+    return self.checkData();
+  }
 
   // define the saveName function
   self.saveName = function(name) {
-    console.log(self.userName);
     self.completeData.name = self.userName;
     $localStorage.completeData.name = self.completeData.name;
   }
 
+  // reset all data models
+  self.resetDataModels = function() {
+    // reset all used models
+    self.newDateString = '';
+    self.timerTags = '';
+    self.timerDescription = '';
+    self.timerHours = 0;
+    self.timerMinutes = 0;
+  }
+
   // define the manual time logger function...
   self.manualTimeLogger = function() {
-    console.log('Button Triggered!');
-  };
 
-  // set the app tagline
-  self.appTag = 'Time management simplified...';
+    var dateString = self.timerDate.toString();
+    for (x = 0; x <= 15; x++) {
+      self.newDateString += dateString[x];
+    }
+    // build up an object
+    var tempObject = {};
+    tempObject.tags = self.timerTags;
+    tempObject.comment = self.timerDescription;
+    tempObject.hour = self.timerHours;
+    tempObject.minute = self.timerMinutes;
+    tempObject.date = self.newDateString;
+
+    // push object into array
+    self.completeData.timeTagged.push(tempObject);
+
+    // reset object for later use
+    tempObject = {};
+
+    // reset all data Models
+    self.resetDataModels();
+  };
 
   // set up the timer functions...
   self.timerFunctions = [];
